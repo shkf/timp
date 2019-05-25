2caf645662d60c4f4bf6ab5b5899a7044a784833
ShishkanovAnton747 <47824374+shkf@users.noreply.github.com>
2019-05-25 06:35:34 +0300

    Update 01_obhodd.c

--- 01_obhodd.c
+++ 01_obhodd.c
@@ -1,199 +1,85 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
 
-typedef struct node
+int sift(int *numbers, int head, int down)
 {
-	int data;
-	struct node * left;
-	struct node * right;
-	struct node * rodit;
-}
-node;
+    int temp = 0;
+    int max, success = 0;
+    while ((head * 2 <= down) && (!success))
+    {
+        if (head * 2 == down) {
+          temp++;
+          max = head * 2;
+        }
+        else if (numbers[head * 2] > numbers[head * 2 + 1]) {
+         temp++; 
+         max = head * 2;
+        }
+        else {temp++;
+          max = head * 2 + 1;
+        }
+         temp++;
+        if (numbers[head] < numbers[max])
+        {
+            int temp1 = numbers[head];
+            numbers[head] = numbers[max];
+            numbers[max] = temp1;
+            head = max;
+            temp++;
+        }
+        else {
+          success = 1;
+             }
 
-typedef struct tree {
-	struct node * Root;
-
-	int count;
-}
-tree;
-
-void init(tree* t)
-{
-	struct tree * new_tree = malloc(sizeof new_tree);
-	new_tree->Root = NULL;
-	new_tree->count = 0;
-	t = new_tree;
-}
-
-int find(tree* t, int zna4, node* n)
-{
-	struct node * s2;
-	s2 = t->Root;
-	if (t->Root == NULL)
-	{
-		return 1;
-	}
-
-	while (1)
-	{
-		if (s2 == NULL)
-		{
-			return 1;
-		}
-		else if (s2->data == zna4)
-		{
-			n->data = s2->data;
-			n->left = s2->left;
-			n->right = s2->right;
-			n->rodit = s2->rodit;
-			return 0;
-		}
-		else if (zna4 > s2->data)
-		{
-			s2 = s2->right;
-		}
-		else
-		{
-			s2 = s2->left;
-		}
-	}
-	return 1;
+    }
+  return temp;
 }
 
-int insert(tree* t, int zna4)
+int funcPyramidSort(int *numbers, int arr_len)
 {
-	struct node * n, ** strrrr, *pr_str = NULL;
-	struct node * e_n;
-	e_n = malloc(sizeof * e_n);
-	int error_tree = find(t, zna4, e_n);
-	if (error_tree == 0)
-	{
-		return 1;
-	}
-	strrrr = &t->Root;
-	n = t->Root;
-	while (1)
-	{
-		if (n == NULL)
-		{
-			n = *strrrr = malloc(sizeof * n);
-			if (n != NULL)
-			{
-				n->data = zna4;
-				n->left = NULL;
-				n->right = NULL;
-				n->rodit = pr_str;
-				t->count++;
-				return 0;
-			}
-			else
-			{
-				return 2;
-			}
-		}
+    int temp = 0;
+    for (int i = (arr_len / 2) ; i >= 0; i--) {
+      temp+=sift(numbers, i, arr_len - 1);
+    }
 
-		pr_str = n;
-		if (zna4 > n->data)
-		{
-			strrrr = &n->right;
-			n = n->right;
-		}
-		else
-		{
-			strrrr = &n->left;
-			n = n->left;
-		}
-	}
-	return 0;
-}
-
-int depth(struct node * n, int deep)
-{
-	if (n == NULL)
-	{
-		return deep;
-	}
-	int depth1 = depth(n->left, deep + 1);
-	int depth2 = depth(n->right, deep + 1);
-	return (depth1 > depth2) ? depth1 : depth2;
-}
-
-void printNode(struct node * n, int current, int deep, int first)
-{
-	if (current == deep)
-	{
-		if (first > 0)
-		{
-			printf(" ");
-		}
-		if (n == NULL)
-		{
-			printf(" ");
-		}
-		else
-		{
-			printf("%d", n->data);
-		}
-	}
-	else if (n != NULL)
-	{
-		printNode(n->left, current + 1, deep, first);
-		printNode(n->right, current + 1, deep, first + 1);
-	}
-	else
-	{
-		printNode(n, current + 1, deep, first);
-		printNode(n, current + 1, deep, first + 1);
-	}
-}
-
-void print(struct node * n)
-{
-
-	int m = depth(n, 0);
-	for (int i = 1; i <= m; i++)
-	{
-		printNode(n, 1, i, 0);
-		printf("\n");
-	}
-}
-
-void printTree(struct tree * t)
-{
-	print(t->Root);
-}
-
-void print_obh1(struct node * n)
-{
-	int m = depth(n, 0);
-	int flag_tree = 0;
-	for (int i = 1; i <= m; i++)
-	{
-		if (flag_tree > 0)
-		{
-			printf(" ");
-		}
-		else
-		{
-			flag_tree++;
-		}
-		printNode(n, 1, i, 0);
-	}
+    for (int i = arr_len - 1; i >= 1; i--)
+    {
+        int temp1 = numbers[0];
+        numbers[0] = numbers[i];
+        numbers[i] = temp1;
+        temp+=sift(numbers, 0, i - 1);
+    }
+    return temp;
 }
 
 int main()
 {
-	struct tree * t = malloc(sizeof t);
-	init(t);
-	for (int i = 0; i< 7; i++)
-	{
-		int a;
-		scanf("%d", &a);
-		insert(t, a);
-	}
-	print_obh1(t->Root);
-	printf("\n");
-	return 0;
+  clock_t start,stop;
+unsigned long t;
+    double rez,sr_rez = 0;
+int n[15] = {1,2,3,4,5,10,15,20,25,30,50,75,100,250,500};
+for (int f = 0 ; f <15;f++)
+{
+int *a;
+a = (int*)malloc(n[f] * sizeof(int));
+start = clock();
+for (int j = 0;j <1000;j++)
+{
+for (int i = 0;i < n[f]; i ++)
+{
+a[i] = rand()%  10000 - 8419;
+} 
+rez = funcPyramidSort(a,n[f]);
+sr_rez +=rez;
+}
+stop = clock();
+printf("%d\n %f \n",n[f],sr_rez/1000);
+sr_rez = 0;
+rez = 0;
+double clock_rez = (stop - start)/(double)CLOCKS_PER_SEC;
+printf("%f \n",clock_rez*100000);
+start ,stop = 0;
+clock_rez = 0;
+}
 }
